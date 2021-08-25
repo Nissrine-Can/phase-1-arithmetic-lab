4dead6d4296518bf92e47bb002817af5cce5d91c
Nissrine Canina <niss.canina@gmail.com>
2021-08-25 13:09:55 -0400

    commit tree 71d0465e34098bc6f0886cee1d5c74c1b0c1ea77

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+multiply;
+
+let random;
+random = Math.ceil(Math.random());
+
+let num3 = 25;
+let num4 =  7;
+const mod = num3 % num4;
+mod;
+
+let max;
+max = Math.max(4, 8, 12, 16, 20);
